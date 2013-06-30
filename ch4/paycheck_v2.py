6d2464ec83033276bc7dde5d1bffd5c7c02ee806
Steven Paul Sanderson II <spsanderson@gmail.com>
2013-06-30 18:44:44 -0400

    added a function to paycheck

---
+++ ch4/paycheck_v2.py
@@ -0,0 +1,33 @@
+def computepay(r, h):
+    print "In computepay", h, r
+    if hours_worked <= 40:
+        p = h * r
+    else:
+        p = r * 40 + (r * 1.5 * (h - 40))
+    print "Finished with computepay", p
+    return p
+
+# ask for hours worked and pay per hour to compute total pay 
+try:
+    hours_worked = float(raw_input('Please enter the hours you worked: \n'))
+    print ''
+except:
+    print 'Please enter hours as a number'
+    quit()
+try:
+    rate_per_hr = float(raw_input('Please enter your hourly rate: \n'))
+    rate_round = round(rate_per_hr, 2)
+    print ''
+except:
+    print 'Please enter rate per hour as a number'
+    quit()
+
+if hours_worked <= 40:
+    pay = hours_worked * rate_round
+else:
+    pay = rate_round * 40 + (rate_round * 1.5 * (hours_worked - 40))
+print 'Your pay is ' + str(pay)
+
+print "In the main code", rate_per_hr, hours_worked
+pay = computepay(rate_per_hr, hours_worked)
+print "We are back", pay
