885daf0606c256c73df8809f6038289a9397213f
Steven Paul Sanderson II <spsanderson@gmail.com>
2013-08-25 10:40:07 -0400

    a program that will print line by line a text file in all UPPER CASE

---
+++ ch7/shout.py
@@ -0,0 +1,9 @@
+'''
+This exercise will write a program that will read through a file
+and print the contents of the file (line by line) all in upper case.
+Executing the program will look as follows:
+
+HERE IS SOME EXAMPLE TEXT 
+THAT WILL BE PRINTED OUT 
+WHEN THIS PROGRAM IS RUN
+'''
