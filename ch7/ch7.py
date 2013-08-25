a05bf27c9f0cf39ceef7f9366164ab56b3b09180
Steven Paul Sanderson II <spsanderson@gmail.com>
2013-08-24 22:04:10 -0400

    chapter 7 material

---
+++ ch7/ch7.py
@@ -0,0 +1,74 @@
+'''Chapter 7 python for informatics code and exercises in this 
+chapter we will start to open up files and work with them
+in their current working directory. You can download a test
+file at the following address:
+http://www.py4inf.com/code/mbox.txt
+'''
+# We will open a file called mbox.txt which is in the director
+# Dropbox/python_for_informatics/ch7
+#fhand = file handle
+fhand = open('mbox.txt')
+print fhand
+
+# New lines
+stuff = 'Hello\nWorld'
+print stuff
+
+stuff = 'x\ny'
+print stuff
+print len(stuff)
+
+# We will now count the lines in the file that we open, we will use
+# fhand from above
+fhand = open('mbox.txt')
+count = 0
+for line in fhand:
+    count = count + 1
+print 'Line count of file:',count
+
+fhand = open('mbox-short.txt')
+inp = fhand.read()
+print len(inp)
+print inp[:20]
+
+fhand = open('mbox-short.txt')
+for line in fhand:
+    if line.startswith('From:'):
+        print line
+        
+fhand = open('mbox-short.txt')
+for line in fhand:
+    line = line.rstrip()
+    if line.startswith('From:'):
+        print line
+
+# The following code will do exactly the same as above
+fhand = open('mbox-short.txt')
+for line in fhand:
+    line = line.rstrip()
+    # skip uninteresting lines
+    if not line.startswith('From:'):
+        continue
+    # Process interesting lines
+    print line
+    
+# Making use of find()
+fhand = open('mbox-short.txt')
+for line in fhand:
+    line = line.rstrip()
+    if line.find('@uct.ac.za') == -1:
+        continue
+    print line
+
+
+# The following will allow a user to specify a particular file name
+# providing it is in the same working directory as the python file
+# itself
+fname = raw_input('Enter the file name: ')
+fhand = open(fname)
+count = 0
+for line in fhand:
+    if line.startswith('Subject:'):
+        count = count + 1
+print 'There were',count,'subject lines in',fname
+
